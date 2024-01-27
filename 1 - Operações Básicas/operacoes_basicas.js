cdf6638d3930e10106aa683643dc40c9265cb72e
Gabr1el-GC <gabrielgc.97@outlook.com>
2024-01-26 22:27:27 -0300

    Adicionando script de operações básicas.

---
+++ 1 - Operações Básicas/operacoes_basicas.js
@@ -0,0 +1,12 @@
+numero1 = 15;
+numero2 = 9;
+
+soma = numero1 + numero2;
+subtracao = numero1 - numero2;
+multiplicacao = numero1 * numero2;
+divisao = numero1 / numero2;
+
+console.log("Soma: ", soma);
+console.log("Subtração: ", subtracao);
+console.log("Multiplicação: ", multiplicacao);
+console.log("Divisão: ", divisao);
