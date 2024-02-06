8de694a3943d70302de0a2191aa71346f1c84e41
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-06 19:55:17 -0300

    Códigos para a operações básicas matemáticas.

--- 1 - Operações Básicas/1.03 - Potência e Raízes.js
+++ 1 - Operações Básicas/1.03 - Potência e Raízes.js
@@ -1,7 +1,8 @@
 let valor1 = 4;
 let valor2 = 3;
 const potencia = Math.pow(valor1, valor2); //Primeira entrada é base e a segunda é expoente.
-const raizQuadrada = Math.sqrt(4); //Ou const raizQuadrada = valor * valor
+const potencia2 = valor1 ** valor2; //Mesma ideia da função Math.pow().
+const raizQuadrada = Math.sqrt(4); //Ou const raizQuadrada = valor * valor.
 const raizOrdemSuperior = Math.pow(valor1, 1/valor2);
 
 console.log("Potência:",potencia);
