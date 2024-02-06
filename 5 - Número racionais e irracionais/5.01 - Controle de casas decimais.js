1ad40dd0379c965adb2e5cad97a7c8f0fc87e744
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-06 02:04:40 -0300

    Para números racionais e irracionais.
    Código serve para ajustar as casas decimais, neste caso para números racionais e irracionais.

---
+++ 5 - Número racionais e irracionais/5.01 - Controle de casas decimais.js
@@ -0,0 +1,8 @@
+//1. Controlar casas decimais
+let racional = 9.1986217982114;
+let racionalDizima = 1/3;
+let irracional = Math.SQRT2;
+
+console.log(racional.toFixed(3));
+console.log(racionalDizima.toFixed(15));
+console.log(irracional.toFixed(25));
