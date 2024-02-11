cdf5b583a59ef544daa8586892ee8175eb971e74
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-10 23:43:02 -0300

    Calcula o seno, cosseno e tangente.

---
+++ 7 - Trigonometria/7.01 - Funções Trigonométricas.js
@@ -0,0 +1,9 @@
+let x = Math.PI / 2; //Obs.: Por padrão o JavaScript interpreta em radianos.
+
+let seno = Math.sin(x);
+let cosseno = Math.cos(x);
+let tangente = Math.tan(x);
+
+console.log(`Seno de ${x}: ${seno}`);
+console.log(`Cosseno de ${x}: ${cosseno}`);
+console.log(`Tangente de ${x}: ${tangente}`);
