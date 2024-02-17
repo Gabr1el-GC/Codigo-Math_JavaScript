d276adb2c70331d723a5a0840621e91f8aa79777
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-16 22:09:27 -0300

    Update 0.06 - Array.js

--- 0 - Básicos do JavaScript/0.06 - Array.js
+++ 0 - Básicos do JavaScript/0.06 - Array.js
@@ -1,3 +1,8 @@
 var MateriaisEscolares = ["Lápis", "Caneta", "Caderno", "Borracha"];
-
 console.log("Os materiais escolares são: " + MateriaisEscolares);
+
+var arrayNumeros = [5, 7, 12]; // Serve para reduzir os elementos de um array a um único valor. 
+var somaValoresNoArray = arrayNumeros.reduce((acumulador, valorAtual) => //Soma os valores da array.
+    acumulador + valorAtual, 0
+);
+console.log(somaValoresNoArray);
