d7adc3d5accd4141560acd2b9cfbccd673c38527
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-20 20:35:26 -0300

    Código que multiplica matrizes.
    Este código realiza uma multiplicação de uma matriz m x n e outra matriz n x p, resultando em uma matriz m x p.

---
+++ 9 - Matrizes/9.04 - Multiplicação de matrizes.js
@@ -0,0 +1,38 @@
+const matrizA = [
+    [1, 3],
+    [5, 7],
+    [9, 11]
+];
+  
+const matrizB = [
+    [2, 4, 6],
+    [8, 10, 12]
+];
+
+function multiplicacaoDeMatrizes(m1, m2) {
+    const resultado = [];
+  
+    if (m1[0].length !== m2.length) { // Verifica se as matrizes podem ser multiplicadas.
+        console.error(`O nº de colunas de ${m1} é diferente do nº de linhas de ${m2}!`);
+        return resultado;
+    }
+
+    for (let i = 0; i < m1.length; i++) { // Inicializando a matriz de resultado
+        resultado[i] = [];
+    }
+  
+    for (let i = 0; i < m1.length; i++) { // Calculando a multiplicação das matrizes
+        for (let j = 0; j < m2[0].length; j++) {
+            let soma = 0;
+            for (let k = 0; k < m2.length; k++) {
+                soma += m1[i][k] * m2[k][j];
+            }
+        resultado[i][j] = soma;
+      }
+    }
+
+    return resultado;
+}
+
+const matrizResultadoMultiplicacao = multiplicacaoDeMatrizes(matrizA, matrizB);
+console.log("Resultado da multiplicação das matrizes:",matrizResultadoMultiplicacao);
