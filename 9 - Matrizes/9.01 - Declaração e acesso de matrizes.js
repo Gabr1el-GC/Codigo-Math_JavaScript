8b9d106b557d03e98f8550eadadade77e143700d
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-20 20:29:17 -0300

    Mostra como declarar e acessar dados das matrizes.

---
+++ 9 - Matrizes/9.01 - Declaração e acesso de matrizes.js
@@ -0,0 +1,13 @@
+const matrizExemplo = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+/* Cada array dentro dos colchetes de'matrizExemplo' é uma linha da matriz. 
+Observe que é uma matriz 3x3. */
+console.log(matrizExemplo);
+
+const acessoMatriz = matrizExemplo[0][2];
+/* Observe que a primeira linha e coluna sempre começam na posição 0.
+Neste exemplo, queremos o elemento a_13 que é 3, ou seja, pelo JavaScript é a_02. */
+console.log(acessoMatriz);
