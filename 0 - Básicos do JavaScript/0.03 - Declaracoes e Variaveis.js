94291c350e2581810aeb54acbc100fb93697ce37
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-21 21:46:35 -0300

    Update 0.03 - Declaracoes e Variaveis.js

--- 0 - Básicos do JavaScript/0.03 - Declaracoes e Variaveis.js
+++ 0 - Básicos do JavaScript/0.03 - Declaracoes e Variaveis.js
@@ -1,13 +1,16 @@
 var Variavel1 = "ExemploValor"; //Inicializa com valor opcionalmente
 var Variavel1 = 500; //Substitui o primeiro valor, ou seja, fez uma conversão
 
-let x = 2500.9; //variável local de escopo do bloco
+let x = 2500.9; //Variável local de escopo do bloco
 
 const Pi = 3.14; //Constante de escopo de bloco apenas para leitura.
 
-var NaoDefinido; //Varíavel sem dados definidos
+var NaoDefinido; //Varíavel sem dados definidos, logo valor 'undefined'.
+
+var Nulo = null; //Variável de valor nulo, ou seja, 'null'.
 
 console.log("Valor de Variavel1: " + Variavel1);
 console.log("Valor de x: " + x);
 console.log("Valor de Pi: " + Pi);
 console.log("Valor de NaoDefinido: " + NaoDefinido);
+console.log("Valor de Nulo: " + Nulo);
