9e1c12ec7814410a5582c2f1a9cb06dfa5c3c15b
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-28 00:02:32 -0300

    Adição do operador '?' e do incrementador '/='.

--- 00 - Básicos do JavaScript/0.09 - Operadores e Incrementadores.js
+++ 00 - Básicos do JavaScript/0.09 - Operadores e Incrementadores.js
@@ -8,6 +8,8 @@ const comparacao5 = valor1 == valor2; /* Compara os valores. Se forem iguais, re
 Se forem diferentes, retorna 'false'. */
 const comparacao6 = valor1 !== valor2; /* Inverso da anterior. Se iguais, 'false'.
 Diferentes, 'true'. */
+const comparacao7 = comparacao1 ? 'Verdadeiro' : 'Falso'; // Realiza uma verificação na condição, e atribui um valor com base nesta condição. 
+const comparacao8 = comparacao2 ? 'Verdadeiro' : 'Falso';
 const incremento1 = valor1 += valor2; // Incrementa o valor2 ao valor1, e depois atribui o resultado ao valor1.
 const incremento2 = valor2 -= valor1; // Subtrai o valor1 do valor2.
 const incremento3 = valor1 *= valor2; // Multiplica o valor1 com valor2.
@@ -26,6 +28,8 @@ console.log(
     comparacao4,
     comparacao5,
     comparacao6,
+    comparacao7,
+    comparacao8,
     incremento1,
     incremento2,
     incremento3,
