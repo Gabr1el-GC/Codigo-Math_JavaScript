8668e07fb9b54cc5e36881dde3fd09f2081d3720
Gabriel G.C <gabrielgc.97@outlook.com>
2024-02-27 23:51:51 -0300

    Adição do 'forEach()'.

--- 00 - Básicos do JavaScript/0.06 - Array.js
+++ 00 - Básicos do JavaScript/0.06 - Array.js
@@ -1,12 +1,17 @@
 var MateriaisEscolares = ["Lápis", "Caneta", "Caderno", "Borracha"];
 console.log("Os materiais escolares são: " + MateriaisEscolares);
+ 
+MateriaisEscolares.forEach(function(MaterialEscolar) { // Itera cada elemento do array.
+    console.log("->", MaterialEscolar); 
+});
 
-var arrayNumeros = [7, 12, 15, 9]; // Serve para reduzir os elementos de um array a um único valor. 
+var arrayNumeros = [7, 12, 15, 9]; // Serve para reduzir os elementos de um array a um único valor.
 var somaValoresNoArray = arrayNumeros.reduce((acumulador, valorAtual) => //Soma os valores da array.
     acumulador + valorAtual, 0 //Neste caso deve-se iniciar o valor acumulado como 1.
 );
 var ordenarValoresdoArray = arrayNumeros.sort((a, b) => //Ordena os valores do array.
     a - b // Neste caso, estamos ordenando em ordem crescente.
 );
+
 console.log(somaValoresNoArray);
 console.log(ordenarValoresdoArray);
