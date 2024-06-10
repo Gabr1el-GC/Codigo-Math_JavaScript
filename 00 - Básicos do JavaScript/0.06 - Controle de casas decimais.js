4cd48c3d4e0bc3c81fa0e542039229f7f45c2fbe
Gabriel G.C. <157774401+Gabr1el-GC@users.noreply.github.com>
2024-06-10 10:50:57 -0300

    Edição das variáveis.

--- 00 - Básicos do JavaScript/0.06 - Controle de casas decimais.js
+++ 00 - Básicos do JavaScript/0.06 - Controle de casas decimais.js
@@ -1,8 +1,14 @@
-//1. Controlar casas decimais
-let racional = 9.1986217982114;
+//Controlar casas decimais
+let inteiro = 21;
+let racional = 9.33333333;
 let DizimaPeriodica = 1/3;
-let irracional = Math.SQRT2;
+let raizQuadrada = Math.SQRT2;
+let numeroE = Math.E;
+let notCientifica = 10 ** -21;
 
-console.log(racional.toFixed(3));
-console.log(DizimaPeriodica.toFixed(15));
-console.log(irracional.toFixed(25));
+console.log(inteiro.toFixed(3));
+console.log(racional.toFixed());
+console.log(DizimaPeriodica.toFixed(5));
+console.log(raizQuadrada.toFixed(10));
+console.log(numeroE.toFixed(20));
+console.log(notCientifica);
